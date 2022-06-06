127296c26cebe28f7ff502c65f76222384ebfcd1
Zenno <zneagu@euroffice.co.uk>
2022-06-06 19:23:06 +0100

    initial commit

---
+++ MvcHelloWorld/MvcHelloWorld.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHelloWorld.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PageOne()
+        {
+            return View();
+        }
+
+        public ActionResult PageTwo()
+        {
+            return View();
+        }
+
+        public ActionResult PageThree()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcHelloWorld/MvcHelloWorld.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcHelloWorld.Web.MvcApplication" Language="C#" %>
